edc7fde8c7a772606e74f99041a388246383b679
Alex Nacimento <alexdavi150@gmail.com>
2023-11-05 18:42:42 -0300

    :(

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -1,6 +1,8 @@
 use yew::prelude::*;
 use wasm_bindgen_futures;
-use reqwasm::http::Request;
+// use reqwasm::http::Request;
+use web_sys::{Request, RequestInit, console, wasm_bindgen::JsValue};
+use reqwest;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
@@ -22,20 +24,45 @@ pub struct Vehicle {
 
 #[function_component]
 pub fn ContactPage() -> Html {
-  let data = use_state(|| None);
-  let cloned_data = data.clone();
+  // let data = use_state(|| None);
+  // let cloned_data = data.clone();  
+  
+  async {
+    // async fn fetch_data() -> Result<(), crate::pages::contact::JsValue> {
+    //   let mut opts = RequestInit::new();
+    //   opts.method("GET");
+    //   let request = Request::new_with_str_and_init("http://localhost:7001/api/vehicle", &opts)?;
+    //   let window = web_sys::window().unwrap();
+    //   wasm_bindgen_futures::JsFuture::from(
+    //     window.fetch_with_request(&request)
+    //   ).await?;
 
-  wasm_bindgen_futures::spawn_local(async move {
-    let result: Vec<Vehicle> = Request::get("http://localhost:7001/api/vehicle")
-      .send()
-      .await
-      .unwrap()
-      .json()
-      .await
-      .unwrap();
+    //   Ok(())
+    // }
 
-    data.set(Some(result.clone()));
-  });
+    // let _ = fetch_data().await;
+
+    let result = reqwest::get("http://localhost:7001/api/vehicle")
+      .await?
+      .text()
+      .await?;
+
+    console::log_1(&JsValue::from(&result));
+  };
+
+  // wasm_bindgen_futures::spawn_local(async move {
+  //   let result: Result<Vec<Vehicle>, _> = gloo_net::http::Request::get("http://localhost:7001/api/vehicle")  
+  //     .send()
+  //     .await
+  //     .unwrap()
+  //     .json()
+  //     .await;
+  //     // .unwrap();
+
+  //     console::log_1(&JsValue::from("smt"));
+
+  //   // data.set(Some(result.clone()));
+  // });
 
   html! {
     <main class="container mx-auto">
@@ -49,12 +76,14 @@ pub fn ContactPage() -> Html {
       >{ "Home page" }</a>
 
       {
-        match cloned_data.as_ref() {
-          Some(d) => {
-            serde_json::to_string(&d).unwrap()
-          },
-          None => "No data found".to_string()
-        }
+        // match cloned_data.as_ref() {
+        //   Some(d) => {
+        //     // serde_json::to_string(&d).unwrap()
+        //     String::from("This is some example data")
+        //   },
+        //   None => "No data found".to_string()
+        // }
+        "Smt"
       }
     </main>
   }
